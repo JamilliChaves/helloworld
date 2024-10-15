978ee78cf4912f0b6e800a7a25e2406c199986eb
JamilliChaves <jamillimarques@outlook.com>
2024-10-15 11:54:55 -0300

    add exercicio

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # helloworld
- meus códigosPython
+ meus códigos Python
---
+++ app01.py
@@ -0,0 +1,20 @@
+#criando uma variavel
+
+'''
+Esse comentario pode ter ,uitas linhas
+'''
+
+"""
+Esse comentário também pode ter muitas linas
+"""
+
+# Cria uma variavel usando "snake case"
+cor_da_casa = 'azul'
+
+#Cria um variavel usando "camelCase"
+corDaCasa = 'roxo'
+
+#exibe as coisas
+print('\n\n\n') # Pula linha
+print(cor_da_casa)
+print(corDaCasa)
